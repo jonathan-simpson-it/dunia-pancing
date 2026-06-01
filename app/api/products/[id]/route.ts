1d092ec9a4ebe8b35f8d175ca8c83280626eaf71
devoob <devanojo2@gmail.com>
2026-06-01 20:59:49 +0800

    prisma

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import type { Prisma } from '.prisma/client'
+import type { Prisma } from '@prisma/client'
 import { prisma } from '@/lib/db'
 import { auth } from '@/lib/auth'
 
